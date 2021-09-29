2fea62cd52f6e92074063d9acdf5a190b22a7a63
Animesh Sinha <AnimeshSinha1309@gmail.com>
2021-09-29 15:29:51 +0530

    Bayesian: Stage 1 of project
    Uploading the basic functional code to run the final project for the Bayesian module.

--- Bayesian/autohire/__main__.py
+++ Bayesian/autohire/__main__.py
@@ -1,11 +1,24 @@
-import pandas as pd
+import numpy as np
 
-from .train import *
+from autohire.utils import parse_pdf, parse_resume_df
+from autohire.bow import BagOfWords
+from autohire.encoder import LabelEncoder
+from autohire.model import BayesianMulticlassModel
 
+if __name__ == "__main__":
+    x_train, y_train = parse_resume_df()
+    bag_of_words = BagOfWords(x_train)
+    label_encoder = LabelEncoder(y_train)
 
-resume_df = pd.read_csv("data/resume-dataset.csv")
-resume_df["Keywords"] = resume_df["Resume"].apply(clean_text)
-resume_df["Tag"], labels = encode_labels(resume_df["Category"])
-word_counts, words = generate_dictionary(resume_df)
+    x_train = bag_of_words.get_counts(x_train)
+    y_train = label_encoder.encode(y_train)
+    model = BayesianMulticlassModel(len(label_encoder), len(bag_of_words))
+    model.fit(x_train=x_train, y_train=y_train)
 
-print(word_counts)
+    x_test = parse_pdf("data/resumes/computers_1.pdf")
+    x_test = bag_of_words.get_counts(x_test)
+    result = model.predict(x_test)
+    result = label_encoder.decode(result)
+
+    for job in result[:5]:
+        print(job)
